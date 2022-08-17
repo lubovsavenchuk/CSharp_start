0ecebc5354650157b4a0cbbace1f45d0cd83adbd
Савенчук Любовь <sav_lu@gmail.com>
2022-08-17 19:46:54 +0500

    Example005_ifelse/

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Type your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mary")
+{
+   Console.WriteLine("Hooray, this is Mary!");
+}
+else
+{
+   Console.Write("Hello, ");
+   Console.WriteLine(username);
+}
