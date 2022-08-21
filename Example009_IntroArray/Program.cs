2053e30327b65d88c352bff1aa155e3a847f7bc8
Савенчук Любовь <sav_lu@gmail.com>
2022-08-21 23:47:50 +0500

    Example009_IntroArray/

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// поиск максимального из 9 чисел с использованием массива
+
+int Max(int arg1, int arg2, int arg3)
+{
+   int result = arg1;
+   if(arg2 > result) result = arg2;
+   if(arg3 > result) result = arg3;
+   return result;
+}
+
+int[] array = {1,302,63,43,5,16,76,8,99};
+int max = Max(
+            Max (array[0], array[1], array[2]),
+            Max (array[3],array[4],array[5]),
+            Max(array[6],array[7],array[9])
+);
+Console.WriteLine(max);
