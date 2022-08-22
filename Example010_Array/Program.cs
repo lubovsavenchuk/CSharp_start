7bc5ff61f510402e2d45b81b55ac3feeeb1a6c03
Савенчук Любовь <sav_lu@gmail.com>
2022-08-22 11:00:02 +0500

    Example010_Array/

---
+++ Example010_Array/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти элемент массива (index) равный find  в одномерном массиве array, состоящем из n элементов.
+
+int[] array = {1,34, 7, 89, 12, 51, 34, 100};
+
+int n = array.Length;
+int find = 34;
+int index = 0;
+
+while (index < n)
+{
+   if (array[index] == find)
+   {
+      Console.WriteLine(index);
+      break;
+   }
+
+   index++;
+} 
